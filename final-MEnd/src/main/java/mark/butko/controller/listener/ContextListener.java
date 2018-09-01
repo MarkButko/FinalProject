559bfb0a93b4feb9237976b510685005c7adf66c
Mark Butko <butko.mark@gmail.com>
2018-09-02 00:54:11 +0300

    annotations used

--- final-MEnd/src/main/java/mark/butko/controller/listener/ContextListener.java
+++ final-MEnd/src/main/java/mark/butko/controller/listener/ContextListener.java
@@ -3,10 +3,12 @@ package mark.butko.controller.listener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@WebListener
 public class ContextListener implements ServletContextListener {
 
 	private static final Logger LOGGER = LogManager.getLogger(ContextListener.class.getName());
--- final-MEnd/src/main/java/mark/butko/controller/listener/SessionListener.java
+++ final-MEnd/src/main/java/mark/butko/controller/listener/SessionListener.java
@@ -1,5 +1,6 @@
 package mark.butko.controller.listener;
 
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
@@ -7,20 +8,28 @@ import javax.servlet.http.HttpSessionListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import mark.butko.model.entity.User;
+
+@WebListener
 public class SessionListener implements HttpSessionListener {
 
 	private static final Logger LOGGER = LogManager.getLogger(SessionListener.class.getName());
 
 	@Override
 	public void sessionCreated(HttpSessionEvent event) {
-		LOGGER.debug("Session Created");
+		LOGGER.info("Session Created");
 		HttpSession session = event.getSession();
 		session.setAttribute("language", "EN");
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
-		LOGGER.debug("Session Destroyed");
+		HttpSession session = se.getSession();
+		User user = (User) session.getAttribute("user");
+		session.removeAttribute("user");
+		session.invalidate();
+		LOGGER.info("Session invalidate for user : {}", user.getEmail());
+		LOGGER.info("Session Destroyed");
 	}
 
 }
