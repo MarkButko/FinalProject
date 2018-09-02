71c7ce61eec2d5b7ccc8d2f2ed3da89310605dd9
Mark Butko <butko.mark@gmail.com>
2018-09-03 02:17:21 +0300

    request redirecting added

--- final-MEnd/src/main/java/mark/butko/controller/command/Command.java
+++ final-MEnd/src/main/java/mark/butko/controller/command/Command.java
@@ -1,7 +1,21 @@
 package mark.butko.controller.command;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public interface Command {
-    String execute(HttpServletRequest request);
+	void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+
+	default void redirect(HttpServletRequest request, HttpServletResponse response, String path)
+			throws IOException {
+		response.sendRedirect(request.getContextPath() + path);
+	}
+
+	default void forward(HttpServletRequest request, HttpServletResponse response, String path)
+			throws ServletException, IOException {
+		request.getServletContext().getRequestDispatcher(path).forward(request, response);
+	}
 }
