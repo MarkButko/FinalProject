e8c94da950de3f4a92d487d49a61ef42deed14f1
Mark Butko <butko.mark@gmail.com>
2018-09-03 02:23:53 +0300

    bugs fixed

--- final-MEnd/src/main/java/mark/butko/controller/command/WelcomeJSPPageCommand.java
+++ final-MEnd/src/main/java/mark/butko/controller/command/WelcomeJSPPageCommand.java
@@ -1,14 +1,17 @@
 package mark.butko.controller.command;
 
+import java.io.IOException;
 import java.util.EnumMap;
 import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import mark.butko.controller.JSPPath;
+import mark.butko.controller.path.JSPPath;
 import mark.butko.model.entity.User;
 
 public class WelcomeJSPPageCommand implements Command {
@@ -20,18 +23,18 @@ public class WelcomeJSPPageCommand implements Command {
 	{
 		path.put(User.Role.ADMIN, JSPPath.ADMIN_PAGE);
 		path.put(User.Role.GUEST, JSPPath.LOGIN);
-		path.put(User.Role.CUSTOMER, JSPPath.PROPOSALS);
+		path.put(User.Role.CUSTOMER, JSPPath.CUSTOMER_PROPOSALS);
 		path.put(User.Role.MANAGER, JSPPath.MASTER_PAGE);
 		path.put(User.Role.MASTER, JSPPath.MASTER_PAGE);
 	}
 
 	@Override
-	public String execute(HttpServletRequest request) {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		User user = (User) request.getSession().getAttribute("user");
 
 		LOGGER.debug("Welcome page for {} : {}", user.getEmail(), path.get(user.getRole()));
 
-		return path.get(user.getRole());
+		forward(request, response, path.get(user.getRole()));
 	}
 
 }
--- final-MEnd/src/main/java/mark/butko/controller/command/WelcomeServletPageCommand.java
+++ final-MEnd/src/main/java/mark/butko/controller/command/WelcomeServletPageCommand.java
@@ -1,14 +1,17 @@
 package mark.butko.controller.command;
 
+import java.io.IOException;
 import java.util.EnumMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import mark.butko.controller.ServletPath;
+import mark.butko.controller.path.JSPPath;
+import mark.butko.controller.path.ServletPath;
 import mark.butko.model.entity.User;
 
 public class WelcomeServletPageCommand implements Command {
@@ -21,17 +24,22 @@ public class WelcomeServletPageCommand implements Command {
 		path.put(User.Role.ADMIN, ServletPath.ADMIN_PAGE);
 		path.put(User.Role.GUEST, ServletPath.LOGIN);
 		path.put(User.Role.CUSTOMER, ServletPath.PAGINATION);
-		path.put(User.Role.MANAGER, ServletPath.MASTER_PAGE);
+		path.put(User.Role.MANAGER, ServletPath.MANAGER_PAGE);
 		path.put(User.Role.MASTER, ServletPath.MASTER_PAGE);
 	}
 
 	@Override
-	public String execute(HttpServletRequest request) {
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		User user = (User) request.getSession().getAttribute("user");
+		String page;
 
-		LOGGER.debug("Welcome page for {} : {}", user.getEmail(), path.get(user.getRole()));
-
-		return path.get(user.getRole());
+		if (user == null) {
+			page = JSPPath.LOGIN;
+		} else {
+			LOGGER.debug("Welcome page for {} : {}", user.getEmail(), path.get(user.getRole()));
+			page = path.get(user.getRole());
+		}
+		redirect(request, response, page);
 	}
 
 }
