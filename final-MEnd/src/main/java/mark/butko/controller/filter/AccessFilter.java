ce94733b8449ceb76129085a215a8f248130a1e2
Mark Butko <butko.mark@gmail.com>
2018-09-03 21:44:55 +0300

    unnecessary objects creation prevented

--- final-MEnd/src/main/java/mark/butko/controller/filter/AccessFilter.java
+++ final-MEnd/src/main/java/mark/butko/controller/filter/AccessFilter.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import mark.butko.controller.command.WelcomeServletPageCommand;
+import mark.butko.controller.command.CommandProvider;
 import mark.butko.controller.path.JSPPath;
 import mark.butko.controller.path.ServletPath;
 import mark.butko.model.entity.User;
@@ -31,6 +31,26 @@ public class AccessFilter implements Filter {
 
 	private static final Logger LOGGER = LogManager.getLogger(AccessFilter.class.getName());
 
+	private static final HashMap<User.Role, String> ROLE_TO_ALLOWED_PATH_MAP = new HashMap<>();
+
+	private static final Set<String> ALLOWED_TO_ALL = new HashSet<>(
+			Arrays.asList(
+					ServletPath.WELCOME_PAGE,
+					ServletPath.SIGN_UP,
+					ServletPath.LOGIN,
+					ServletPath.EMPTY,
+					ServletPath.LOGOUT,
+					JSPPath.SIGN_UP,
+					JSPPath.LOGIN));
+
+	static {
+		ROLE_TO_ALLOWED_PATH_MAP.put(Role.CUSTOMER, "customer");
+		ROLE_TO_ALLOWED_PATH_MAP.put(Role.MANAGER, "manager");
+		ROLE_TO_ALLOWED_PATH_MAP.put(Role.MASTER, "master");
+		ROLE_TO_ALLOWED_PATH_MAP.put(Role.ADMIN, "admin");
+		ROLE_TO_ALLOWED_PATH_MAP.put(Role.GUEST, "guest");
+	}
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		LOGGER.info("init");
@@ -39,10 +59,12 @@ public class AccessFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
-		LOGGER.trace("path : {}", ((HttpServletRequest) req).getRequestURI());
+
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-		String requestedPath = request.getRequestURI().substring(ServletPath.SERVLET_CONTEXT.length());
+
+		String requestedPath = request.getRequestURI()
+				.substring(ServletPath.SERVLET_CONTEXT.length());
 
 		if (requestedPath.matches(".*(css|jpg|png|gif|js|woff|woff2|ttf|map)$")) {
 			chain.doFilter(request, response);
@@ -58,12 +80,13 @@ public class AccessFilter implements Filter {
 		}
 
 		LOGGER.debug("requestedPath = {}, user.role = {}", requestedPath, user.getRole().name());
+
 		boolean hasRights = checkRights(user.getRole(), requestedPath);
 		if (hasRights && !requestedPath.endsWith(".jsp")) {
 			chain.doFilter(request, response);
 		} else {
 			LOGGER.info("access denied for user : {} {} on url : {}", user.getEmail(), user.getRole(), requestedPath);
-			new WelcomeServletPageCommand().execute(request, response);
+			CommandProvider.getCommandOrWelcomePage("").execute(request, response);
 		}
 	}
 
@@ -73,29 +96,6 @@ public class AccessFilter implements Filter {
 	}
 
 	private boolean checkRights(User.Role role, String path) {
-
-		HashMap<User.Role, String> roleToPathMap = new HashMap<>();
-
-		Set<String> allowedToAll = new HashSet<>(
-				Arrays.asList(
-						ServletPath.WELCOME_PAGE,
-						ServletPath.SIGN_UP,
-						ServletPath.LOGIN,
-						ServletPath.EMPTY,
-						ServletPath.LOGOUT,
-						JSPPath.SIGN_UP,
-						JSPPath.LOGIN));
-
-		roleToPathMap.put(Role.CUSTOMER, "customer");
-		roleToPathMap.put(Role.MANAGER, "manager");
-		roleToPathMap.put(Role.MASTER, "master");
-		roleToPathMap.put(Role.ADMIN, "admin");
-		roleToPathMap.put(Role.GUEST, "guest");
-
-		if (allowedToAll.contains(path) || path.contains(roleToPathMap.get(role))) {
-			return true;
-		}
-
-		return false;
+		return (ALLOWED_TO_ALL.contains(path) || path.contains(ROLE_TO_ALLOWED_PATH_MAP.get(role)));
 	}
 }
